02de94fa69ba1ec91d54fb35d4aca0b7c614b2aa
Mark Wang <mwwang@gmail.com>
2013-03-17 17:10:12 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xlearn::Application.config.secret_token = '59941cb7ea0de406a7bf6f01948e8840ad2a8995d8f5bb3162afa7e4c7f81ad753a8d19491299cf89e72baf3940f468e90c6c7419f737108f59f2cfdca143849'
