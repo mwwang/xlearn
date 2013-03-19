6d9ab13c4a0b2be5826e53beabd3bad121e345f7
Mark Wang <mwwang@gmail.com>
2013-03-19 11:15:35 +0800

    Layouts and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Xlearn::Application.routes.draw do
   root to: 'static_pages#home'
 
+  match '/about', to: 'static_pages#about'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
