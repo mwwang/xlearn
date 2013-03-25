9be560dbbba36d92034549e7a1407f0b96b53700
Mark Wang <mwwang@gmail.com>
2013-03-25 16:45:28 +0800

    user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+	before_filter :authenticate_user!, only: [:index, :edit, :update]
+
+	def index
+		@users = User.all
+	end
+
   def show
-  	@user = current_user
+  	@user = User.find(params[:id])
+  	# @user = current_user
   end
 end
