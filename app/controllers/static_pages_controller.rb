ccd85469d383c9f19a02483ae4b50e22d8dc8921
Mark Wang <mwwang@gmail.com>
2013-03-27 20:59:59 +0800

    start of relationship

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
 
   def home
-  	@post = current_user.posts.build if user_signed_in?
-  	@feed_items = current_user.feed.paginate(page: params[:page])
+  	if user_signed_in?
+  		@post = current_user.posts.build
+  		@feed_items = current_user.feed.paginate(page: params[:page])
+  	end
   end
 end
