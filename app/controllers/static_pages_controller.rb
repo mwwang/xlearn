9a53522d8396a6653f709702a8553fafc840aaa3
Mark Wang <mwwang@gmail.com>
2013-04-06 22:46:17 +0800

    added well and semi form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+  	@users = User.all
   	if user_signed_in?
   		@post = current_user.posts.build
   		@feed_items = current_user.feed.paginate(page: params[:page])
